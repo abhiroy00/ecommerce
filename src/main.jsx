59fd408ba4b64408d3ae4814ee1259eb0f3b0bde
khdatwani <khdatwani@gmail.com>
2024-12-13 22:32:23 +0530

    Login Button
    Work in progress , did some changes in main.jsx.

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,25 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import store from './store.js'
+import { Auth0Provider } from '@auth0/auth0-react';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+  <Auth0Provider
+    domain="dev-ayhqugvs12u1m0zk.us.auth0.com"
+    clientId="vRzeO0zPo42AgS6BtQT8W6w6lPLEyrun"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <Provider store={store}>
     <App />
     </Provider>
-  </StrictMode>,
+    </Auth0Provider>
+    </StrictMode>
+  
 )
