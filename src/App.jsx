6691d2dba1a6ad662cc8dce25627c2e2c0498aa7
ashishthakur113 <ashthakur113@gmail.com>
2024-12-13 17:47:00 +0530

    changes
    i just added my name

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
       <h1>hii this is ecommerce website</h1>
       <h2>hello how are you ?</h2>
       <h2>my name is Pritam mann</h2>
+      <h1>hii i am Ashish </h1>
     </>
   )
 }
