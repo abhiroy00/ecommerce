66c9af072f2c220415864c9df787838edbbb44ab
abhiroy00 <abhi7464099@gmail.com>
2024-12-13 17:17:05 +0530

    first-commit
    i chnage in app or store file where problem in configure file

--- src/store.js
+++ src/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './reducers';
+
 
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    
+  }
+ 
 });
 
 export default store
