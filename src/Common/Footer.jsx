d89baf240adcf8d4d403f2401db1098345a12aac
Pritam mann <preetam1202204@gmail.com>
2024-12-14 22:18:44 +0530

    footer media query update

--- src/Common/Footer.jsx
+++ src/Common/Footer.jsx
@@ -6,37 +6,37 @@ export default function Footer() {
   return (
     <div className='bg-black h-auto'>
         <div className='text-center'>
-          <h1 className='text-[4vh]  text-amber-600  '><span className="text-amber-400">Quick</span>Buy</h1>
+          <h1 className='text-[4vh] lg:text-5xl text-amber-600 mb-5 '><span className="text-amber-400">Quick</span>Buy</h1>
         </div>
-      <div className='flex justify-between h-auto mb-0'>
+      <div className='flex justify-between lg:justify-around h-auto mb-0'>
         <div className='mb-10'>
-          <h1 className='text-[2vh] text-white'>Connect with
+          <h1 className='text-[2vh] lg:text-3xl text-white text-center'>Connect with
             us</h1>
           <ul className='flex justify-center flex-col mt-5 items-center'>
-            <li className='text-[1vh] inline text-zinc-400'>Facebook </li>
-            <li className='text-[1vh] text-zinc-400'>Twitter </li>
-            <li className='text-[1vh] text-zinc-400'>Instagram </li>
-            <li className='flex gap-1 text-zinc-400'><FaFacebook className='text-blue-700 text-[1vh]'/>
-              <FaTwitter className='text-blue-700 text-[1vh]' /> < FaInstagram  className=' text-blue-800 text-[1vh]' /></li>
+            <li className=' text-[1vh] lg:text-2xl inline text-zinc-400'>Facebook </li>
+            <li className=' text-[1vh] lg:text-2xl text-zinc-400'>Twitter </li>
+            <li className=' text-[1vh] lg:text-2xl text-zinc-400'>Instagram </li>
+            <li className='flex gap-1 text-zinc-400'><FaFacebook className='text-blue-700  text-[1vh] lg:text-2xl'/>
+              <FaTwitter className='text-blue-700  text-[1vh] lg:text-2xl' /> < FaInstagram  className=' text-blue-800  text-[1vh] lg:text-2xl' /></li>
 
           </ul>
         </div>
         <div className='mb-10'>
-          <h4 className='text-[2vh] text-white'>Make Money with Us</h4>
+          <h4 className='text-[2vh] lg:text-3xl text-white text-center'>Make Money with Us</h4>
           <ul className='flex justify-center flex-col mt-5 items-center'>
-            <li className='text-[1vh]  text-zinc-400'>Sell on Amazon</li>
-            <li className='text-[1vh] text-zinc-400'>supply to Amazon</li>
-            <li className='text-[1vh] text-zinc-400'>Become an Affiliate</li>
-            <li className='text-[1vh] text-zinc-400'>Advertise Your Products</li>
+            <li className=' text-[1vh] lg:text-2xl  text-zinc-400'>Sell on Amazon</li>
+            <li className=' text-[1vh] lg:text-2xl text-zinc-400'>supply to Amazon</li>
+            <li className=' text-[1vh] lg:text-2xl text-zinc-400'>Become an Affiliate</li>
+            <li className=' text-[1vh] lg:text-2xl text-zinc-400'>Advertise Your Products</li>
           </ul>
         </div>
         <div className='mb-10'>
-          <h4 className='text-[2vh] text-white'>Let Us Help You</h4>
+          <h4 className='text-[2vh] lg:text-3xl text-white text-center'>Let Us Help You</h4>
           <ul className='flex justify-center flex-col mt-5 items-center'>
-            <li className='text-[1vh]  text-zinc-400'>Your Account</li>
-            <li className='text-[1vh] text-zinc-400'>Returns Center</li>
-            <li className='text-[1vh] text-zinc-400'>100% Purchase Protection</li>
-            <li className='text-[1vh] text-zinc-400'>Help Center</li>
+            <li className=' text-[1vh] lg:text-2xl  text-zinc-400'>Your Account</li>
+            <li className=' text-[1vh] lg:text-2xl text-zinc-400'>Returns Center</li>
+            <li className=' text-[1vh] lg:text-2xl text-zinc-400'>100% Purchase Protection</li>
+            <li className=' text-[1vh] lg:text-2xl text-zinc-400'>Help Center</li>
           </ul>
         </div>
       </div>
