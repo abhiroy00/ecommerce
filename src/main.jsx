863b4f21509e592057d1f57374297ff61c3d4f8c
khdatwani <khdatwani@gmail.com>
2024-12-14 12:54:06 +0530

    Login/Logout Function Complete
    Login/ logout function complete , installed react-router-dom too

--- src/main.jsx
+++ src/main.jsx
@@ -4,22 +4,22 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import store from './store.js'
-// import { Auth0Provider } from '@auth0/auth0-react';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  {/* <Auth0Provider
+  <Auth0Provider
     domain="dev-ayhqugvs12u1m0zk.us.auth0.com"
     clientId="vRzeO0zPo42AgS6BtQT8W6w6lPLEyrun"
     authorizationParams={{
       redirect_uri: window.location.origin
     }}
-  > */}
+  >
     <Provider store={store}>
     <App />
     </Provider>
-    {/* </Auth0Provider> */}
+    </Auth0Provider>
     </StrictMode>
   
 )
