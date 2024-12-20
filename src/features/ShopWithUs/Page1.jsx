631a3e9d3f354427e1aa4f5e4a0bf5e71633e40b
khdatwani <khdatwani@gmail.com>
2024-12-20 12:25:25 +0530

    added my order features
    My order page added , 3 pages added

---
+++ src/features/ShopWithUs/Page1.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const LandingPage = () => {
+  return (
+    <div className="bg-gray-900 text-white">
+      <div className="container mx-auto py-16 px-4">
+        <div className="text-center">
+          
+          <h1 className="text-4xl font-bold mb-4">Super Fast and Free Delivery</h1>
+          <p className="text-lg mb-8">Enjoy free and fast delivery on all your favorite products!</p>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div>
+            <h2 className="text-2xl font-bold mb-4">Why Choose Us?</h2>
+            <ul className="list-none p-0">
+              <li className="mb-4">Lightning-fast delivery</li>
+              <li className="mb-4">Free shipping on all orders</li>
+              <li className="mb-4">Easy returns and exchanges</li>
+              <li className="mb-4">Secure checkout process</li>
+            </ul>
+          </div>
+          <div className="text-center">
+           <Link to='/Product'> <button className="bg-yellow-500 hover:bg-yellow-600 text-black font-bold py-4 px-8 rounded-full">Shop Now</button></Link>
+            <button className="bg-gray-700 hover:bg-gray-800 text-white font-bold py-4 px-8 rounded-full mt-4">Learn More</button>
+          </div>
+        </div>
+      </div>
+
+      <footer className="bg-gray-800 text-gray-400 py-4 text-center">
+        <p>&copy; QuickBuy</p>
+      </footer>
+    </div>
+  );
+};
+
+export default LandingPage;
---
+++ src/features/ShopWithUs/Page2.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div className="bg-gray-900 text-white">
+      <div className="container mx-auto py-16 px-4">
+        <div className="bg-blue-500 text-white p-6 rounded-lg mb-8">
+          <div className="text-center">
+            <h2 className="text-2xl font-bold mb-4">$</h2>
+            <h3 className="text-lg font-bold mb-2">Money-back Guaranteed</h3>
+            <p>30-day money-back guarantee.</p>
+          </div>
+        </div>
+
+        </div>
+
+      <footer className="bg-gray-800 text-gray-400 py-4 text-center">
+        <p>&copy; QuickBuy</p>
+      </footer>
+    </div>
+  );
+};
+
+export default LandingPage;
