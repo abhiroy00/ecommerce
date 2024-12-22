0e1fdbe06bad26e3cf7680937b329e5da795b7d2
stembedded7309 <stembedded@gmail.com>
2024-12-22 16:02:24 +0530

    productapp created and orientation
    set orientation  of database function.

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -88,9 +88,9 @@ DATABASES = {
         				'NAME': 'quickbuy',          
        					'USER': 'quickbuy',           
         				'PASSWORD': 'quickbuy',     
-        			'HOST': 'localhost',          
-        'PORT': '3306',               
-    }
+        			    'HOST': 'localhost',          
+                        'PORT': '3306',               
+                }
 }
 
 # Password validation
