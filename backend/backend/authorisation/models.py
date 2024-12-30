38ab1fcaa7051e26adc3635b2e6eb57bcc99f8ea
Kumar-vikalp <145014101+Kumar-vikalp@users.noreply.github.com>
2024-12-30 11:43:16 +0530

    removed first_name and last_name from model
    Now just need email and password to register an user.

--- backend/backend/authorisation/models.py
+++ backend/backend/authorisation/models.py
@@ -12,18 +12,12 @@ from django.utils.timezone import now
 
 
 class User(AbstractBaseUser , PermissionsMixin):
-
-    username = None
     
     email = models.EmailField(
         max_length=255,
         unique=True,
         verbose_name=_("email address"),
     )
-
-    first_name = models.CharField(max_length=100, verbose_name=_("Enter the First name here:"))
-    last_name = models.CharField(max_length=100, verbose_name=_("Enter the Last name here"))
-
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     is_verified = models.BooleanField(default=False)
@@ -35,12 +29,7 @@ class User(AbstractBaseUser , PermissionsMixin):
     otp = models.CharField(max_length=6 , null=True, blank=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ["first_name", "last_name"]
     
     objects = UserManager()
-    
-    def get_full_name(self):
-        return f"{self.first_name} {self.last_name}"
-
     def __str__(self):
         return self.email
--- backend/backend/authorisation/serializer.py
+++ backend/backend/authorisation/serializer.py
@@ -4,14 +4,12 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['email', 'is_verified', 'first_name', 'last_name', 'password']
+        fields = ['email', 'password', 'is_verified']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = User(
             email=validated_data['email'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
         )
         
         user.set_password(validated_data['password'])  # Hash the password
